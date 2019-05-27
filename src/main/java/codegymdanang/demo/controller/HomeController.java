22685294c24ff1ed4239951304aa4dce2d24c455
codegymdanang <codegymdanang@gmail.com>
2019-05-27 16:34:43 +0700

    init project

---
+++ src/main/java/codegymdanang/demo/controller/HomeController.java
@@ -0,0 +1,22 @@
+package codegymdanang.demo.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+
+    Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @RequestMapping("/")
+    public String homePage() {
+
+        logger.debug("Vo Day");
+        return "home";
+    }
+
+}
